24700cc2235cc1672e40991ee3dc5abdcce8ffc7
anton thunman jonsson <jonssonanton@hotmail.com>
2021-03-15 09:52:52 +0100

    TrackOrm
    Changed foreach to for.

--- Source/TrainEngine.Tests/TrackOrmTests.cs
+++ Source/TrainEngine.Tests/TrackOrmTests.cs
@@ -69,4 +69,4 @@ namespace TrainEngine.Tests
         }
         }
     }
-}
+
--- Source/TrainEngine/TrackOrm.cs
+++ Source/TrainEngine/TrackOrm.cs
@@ -8,9 +8,15 @@ namespace TrainEngine
         {
             TrackDescription trackDescription = new TrackDescription();
             char[] chars = track.ToCharArray();
-            foreach(var ch in chars)
+            for(int i = 0; i < chars.Length; i++)
             {
-                if ch =
+                if(chars[i] == '[')
+                {
+                    var stations = TravelPlan.GetStations(@"Data\stations.txt");
+                    var id = Convert.ToInt32(chars[i + 1]);
+                    var station =  stations.fi
+                    trackDescription.Parts.Add(new Station());
+                }
             }
 
         }
